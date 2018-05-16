31d74ad041ed2f566f2e1dcaa121b91649d0805d
Nicolas Hiillos <nicolashiillos@gmail.com>
2018-05-16 19:20:59 +0300

    IT WORKS

--- ytdl/songs/views.py
+++ ytdl/songs/views.py
@@ -6,22 +6,24 @@ from rest_framework.response import Response
 
 from .serializers import SongSerializer
 #from .api import SongViewSet
-from .ytdlWrapper import dl_url
+from .ytdlWrapper import dl_url as ytdl
 
 @api_view(['POST'])
 def dl_url(request):
     try:
-        dbdata = dl_url(request.data['url'], './test/')
+        dbdata = ytdl(request.data['url'], './test/')
+        print(dbdata)
         serializer = SongSerializer(data=dbdata)
         if serializer.is_valid():
             serializer.save()
             return Response({request.data['url']: 'DONE'})
-            #return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        #return Response({request.data['url']: 'ERROR'})
+        return Response({request.data['url']: 'ERROR'})
     except:
         print("ERROR IN URL")
         return Response({request.data['url']: 'ERROR'})
+#    dbdata = dl_url('https://www.youtube.com/watch?v=VYOjWnS4cMY', '/home/sakamoto/test/')
+            #return Response(serializer.data, status=status.HTTP_201_CREATED)
+#        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         #return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     #return Response({"test": 'abc'})
     #return Response({"msg": 'INVALID URL'})
--- ytdl/songs/ytdlWrapper.py
+++ ytdl/songs/ytdlWrapper.py
@@ -1,4 +1,4 @@
-import sys
+#import sys
 import youtube_dl
 
 #global variables
