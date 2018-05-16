9d64cb7ee0849ef364ccc88de1c829397d59b01a
Nicolas Hiillos <nicolashiillos@gmail.com>
2018-05-16 22:34:50 +0300

    fix to ytdl parse issue

--- ytdl/songs/ytdlWrapper.py
+++ ytdl/songs/ytdlWrapper.py
@@ -1,3 +1,4 @@
+from __future__ import unicode_literals
 #import sys
 import youtube_dl
 
@@ -17,10 +18,11 @@ class MyLogger(object):
 
 def handle_finished(d):
     #turn video filename into .mp3 (youtubedl gives filename before conversion to mp3)
-    filename = d['filename'].split('/')[-1].split('.')[0] 
+    filename1 = d['filename'].split('/')[-1].split('.')[0] 
+    filename2 = d['filename'].split('/')[-1]
     global data
-    data['filename'] = filename + '.mp3'
-    data['title'] = filename.split('-')[1].strip()
+    data['filename'] = filename2
+    data['title'] = filename1
     #print()
 
 def my_hook(d):
@@ -36,13 +38,17 @@ def dl_url(url, direc):
     data['url'] = url
     ydl_opts = {
         'format': 'bestaudio/best',
+        #'format': 'best',
         'download_archive': direc + 'downloaded_songs.txt',
         'outtmpl': direc + '%(title)s.%(ext)s',
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
+            #'key': 'AvConvExtractAudio',
             'preferredcodec': 'mp3',
-            'preferredquality': '320',
+            #'preferredcodec': 'libmp3lame',
+            'preferredquality': '192',
         }],
+        'quiet': 'False',
         'logger': MyLogger(),
         'progress_hooks': [my_hook],
     }
