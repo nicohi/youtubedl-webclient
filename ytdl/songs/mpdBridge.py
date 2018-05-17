20386a0939584b6acb197af4f619b0827f45c42d
Nicolas Hiillos <nicolashiillos@gmail.com>
2018-05-17 08:56:05 +0300

    mpd integration testing

---
+++ ytdl/songs/mpdBridge.py
@@ -0,0 +1,15 @@
+import mpd
+
+def que_song(song, ip, port):
+    client = mpd.MPDClient()
+    client.connect(ip, port)
+    client.add('song')
+    print(client.playlist())
+    client.disconnect()
+    #client = mpd.MPDClient(use_unicode=True)
+    #client.connect("localhost", 6600)
+    #client.add('file:///home/sakamoto/music/01 - Moth.flac')
+    #for entry in client.lsinfo("/"):
+    #    print("%s" % entry)
+    #for key, value in client.status().items():
+    #    print("%s: %s" % (key, value))
--- ytdl/songs/views.py
+++ ytdl/songs/views.py
@@ -9,16 +9,18 @@ from .serializers import SongSerializer
 from .api import SongViewSet
 
 from .ytdlWrapper import dl_url as ytdl
+from .mpdBridge import que_song
 
 @api_view(['POST'])
 def dl_url(request):
     try:
         dbdata = ytdl(request.data['url'], './test/')
         print(dbdata)
-        print(sys.argv)
         serializer = SongSerializer(data=dbdata)
         if serializer.is_valid():
             print(serializer.save())
+            if(sys.argv[-1] is 'mopidy'):
+                que_song('localhost', 6600, 'file:///home/sakamoto/prog/projects/youtubedl-webclient/ytdl/test/' + dbdata['filename'])
             return Response(serializer.data, status=status.HTTP_201_CREATED)
             #return Response({request.data['url']: 'DONE'})
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
